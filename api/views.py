2a62228921859f47225b1b6f2d01ec389d59ee5d
oliverbarkerWork <oliverbarker@aarsleff.co.uk>
2021-06-07 16:59:32 +0100

    project initialized with basic API and sqlite added.

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Users
+from .serializers import UserSerializers
+from rest_framework import generics
+
+# Create your views here.
+
+class UserView(generics.CreateAPIView):
+    queryset = Users.objects.all()
+    serializer_class = UserSerializers
