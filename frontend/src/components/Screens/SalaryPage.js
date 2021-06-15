d261fdc1da4840e18921f7b7e097c56bed5e5a4c
oliverbarkerWork <oliverbarker@aarsleff.co.uk>
2021-06-15 14:31:52 +0100

    salary form css reworked to stop element overlapping on resizing

--- frontend/src/components/Screens/SalaryPage.js
+++ frontend/src/components/Screens/SalaryPage.js
@@ -5,33 +5,29 @@ import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import { salaryValidationSchema } from "../Validation/ValidationSchema";
 
-const validationSchema = salaryValidationSchema
 const useStyles = makeStyles({
-
   btn: {
-    background: '#1F85DE',
+    background: "#1F85DE",
     border: 0,
     borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    margin: '5%',
-    padding: '0 25%',
+    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+    color: "white",
+    height: 120,
+    width: "100%",
+    marginTop: "5%",
+    padding: "0 25%",
+    fontSize: "4rem"
   },
   field: {
-     width: '100%',
+    width: "100%",
+    height: "25%",
+    fontSize: "4rem",
+    paddingBottom: 10,
   },
-  fieldContainer: {
-    display: 'flexbox',
-    justifyContent: 'center',
-    padding: '2rem'
-  },
-  dateStyle: {
-    width: '180px',
-
-  }
 
 });
+
+const validationSchema = salaryValidationSchema
 
 const SalaryPage = () => {
   const formik = useFormik({
@@ -63,79 +59,90 @@ const SalaryPage = () => {
   const classes = useStyles();
 
   return (
-    <div>
-    <body class="main">
-      <header class="navbar">
-        <a href="https://www.aarsleff.co.uk/">
-          <img class ="logo" src="https://forms.aarsleff.co.uk/images/Logo.png?v=gNMJrA7Q8A" width='92px' height='51px' />
-        </a>
-      </header>
-    <div class="container">
-      <h1 class="title">Employee Salary</h1>
-    <div class="d1">
-      
-    <form onSubmit={formik.handleSubmit} >
-      <div class={classes.fieldContainer} >
-        <TextField
-          className = {classes.field}
-          label="Employee ID"
-          name="employeeID"
-          type="text"
-          value={formik.values.employeeID}
-          placeholder="1234"
-          onChange={formik.handleChange}
-          error={formik.touched.employeeID && Boolean(formik.errors.employeeID)}
-          helperText={formik.touched.employeeID && formik.errors.employeeID}
-        />
-        <div className="spacer" />
+    <body>
+      <div class="grid-container">
+        <header class="header">
+          <a href="https://www.aarsleff.co.uk/">
+            <img
+              class="logo"
+              src="https://forms.aarsleff.co.uk/images/Logo.png?v=gNMJrA7Q8A"
+              width="92px"
+              height="51px"
+            />
+          </a>
+        </header>
 
-        <TextField
-          className = {classes.field}
-          label="Employee Name"
-          name="employeeName"
-          type="text"
-          value={formik.values.employeeName}
-          placeholder="frank"
-          error={
-            formik.touched.employeeName && Boolean(formik.errors.employeeName)
-          }
-          onChange={formik.handleChange}
-          helperText={formik.touched.employeeName && formik.errors.employeeName}
-        />
-        <div className="spacer" />
-        <TextField
-          className = {classes.field}
-          label="Weekly Salary"
-          name="salaryWeekly"
-          type="text"
-          value={formik.values.salaryWeekly}
-          placeholder="2345"
-          error={formik.touched.salaryWeekly && Boolean(formik.errors.salaryWeekly)}
-          onChange={formik.handleChange}
-          helperText={formik.touched.salaryWeekly && formik.errors.salaryWeekly}
-        />
-        <div className="spacer" />
+        <main class="main">
+        <h1 class="title">Employee Timesheets</h1>
         
-        <div className="dateStyle">
-        <TextField
-          className = {classes.field}
-          // label="date"
-          name="date"
-          type="date"
-          value={formik.values.date}
-          error={formik.touched.date && Boolean(formik.errors.date)}
-          onChange={formik.handleChange}
-          // helperText={formik.touched.date && formik.errors.date}
-        />
-        </div>
+          <form onSubmit={formik.handleSubmit}>
+            <div class={classes.fieldContainer}>
+              <div className="spacer" />
+
+              <TextField
+              id="outlined-basic" 
+               variant="outlined"
+                className={classes.field}
+                label="Employee ID"
+                name="employeeID"
+                type="text"
+                value={formik.values.employeeID}
+                placeholder="2345"
+                error={
+                  formik.touched.employeeID && Boolean(formik.errors.employeeID)
+                }
+                onChange={formik.handleChange}
+                helperText={
+                  formik.touched.employeeID && formik.errors.employeeID
+                }
+              />
+
+              <div className="spacer" />
+
+              <TextField
+              id="outlined-basic" 
+              variant="outlined"
+                className={classes.field}
+                label="Employee Name"
+                name="employeeName"
+                type="text"
+                value={formik.values.employeeName}
+                placeholder="2345"
+                error={
+                  formik.touched.employeeName &&
+                  Boolean(formik.errors.employeeName)
+                }
+                onChange={formik.handleChange}
+                helperText={
+                  formik.touched.employeeName && formik.errors.employeeName
+                }
+              />
+
+              <div className="spacer" />
+
+              <TextField
+              id="outlined-basic" 
+              variant="outlined"
+                className={classes.field}
+                label="Weekly Salary"
+                name="salaryWeekly"
+                type="text"
+                value={formik.values.salaryWeekly}
+                placeholder="2345"
+                error={formik.touched.salaryWeekly && Boolean(formik.errors.salaryWeekly)}
+                onChange={formik.handleChange}
+                helperText={formik.touched.salaryWeekly && formik.errors.salaryWeekly}
+              />
+             
+              </div>
+            <Button type="submit" color="primary" className={classes.btn}>
+              Submit
+            </Button>
+          </form>
+        </main>
+        <footer class="footer">All right reserved.</footer>
       </div>
-      <Button type="submit" color="primary" className = {classes.btn}>Submit</Button>
-    </form>
-    </div>
-    </div>
     </body>
-    <footer class="foot" />
-    </div>
   );
 };
 
